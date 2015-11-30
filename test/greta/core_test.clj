2dce16f0b98672af78a2580f5d3c015b8e57207d
Robert Crim <rob@servermilk.com>
2015-11-30 15:28:42 -0800

    Move tests of group offsets

--- test/greta/core_test.clj
+++ test/greta/core_test.clj
@@ -83,3 +83,42 @@
              (get-in
               @(s/take! c)
               [0 :partitions 0 :error-code]))))))
+
+
+(deftest offset-commit-test'
+  (let [r {:header {:api-key :offset-commit
+                    :api-version 1
+                    :correlation-id 1
+                    :client-id "greta-test"}
+           :consumer-group-id "my-group"
+           :consumer-group-generation-id 1
+           :consumer-id "my-group"
+           :retention-time -1 ;; would like to NOT set this?
+           :topics [{:topic "greta-tests"
+                     :partitions [{:partition-id 0
+                                   :offset 101
+                                   :timestamp (System/currentTimeMillis)
+                                   :metadata "funky"}]}]}]
+
+    (with-open [c @(client "localhost" 9092)]
+      @(s/put! c r)
+      (is (= :illegal-generation
+             (get-in @(s/try-take! c 1000)
+                     [:topics 0 :partitions 0 :error-code]))))))
+
+
+(deftest offset-fetch-test'
+  (let [r {:header {:api-key :offset-fetch
+                    :api-version 1
+                    :correlation-id 1
+                    :client-id "greta-test"}
+           :consumer-group "my-group"
+           :topics [{:topic "greta-tests"
+                     :partitions [0]}]}]
+
+    (with-open [c @(client "localhost" 9092)]
+
+      @(s/put! c r)
+      (is (= :none
+             (get-in @(s/try-take! c 1000)
+                     [:topics 0 :partitions 0 :error-code]))))))
